492eb2922f837e8b3585252ce9f2fba86f3cb132
Teodor <tedko_97@abv.bg>
2016-02-24 07:48:38 +0200

    Add administration for users

---
+++ Source/Web/Organizr.Web/Areas/Administration/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿namespace Organizr.Web.Areas.Administration.Controllers
+{
+    using System.Web.Mvc;
+    using Infrastructure.Mapping;
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+    using MvcTemplate.Services.Data;
+    using ViewModels.Users;
+
+    [Authorize(Roles = "Administrator")]
+    public class UsersController : Controller
+    {
+        private IUsersServices users;
+
+        public UsersController(IUsersServices users)
+        {
+            this.users = users;
+        }
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        public ActionResult Users_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            DataSourceResult result = this.users
+                .GetAll()
+                .To<UserViewModel>()
+                .ToDataSourceResult(request);
+
+            return this.Json(result);
+        }
+    }
+}
---
+++ Source/Web/Organizr.Web/Areas/Administration/ViewModels/Users/UserViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Organizr.Web.Areas.Administration.ViewModels.Users
+{
+    using System.ComponentModel;
+    using AutoMapper;
+    using Data.Models;
+    using Infrastructure.Mapping;
+
+    public class UserViewModel : IMapFrom<User> , IHaveCustomMappings
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [DisplayName("Events visited")]
+        public string EventsParticipatedCount { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<User, UserViewModel>()
+                .ForMember(x => x.EventsParticipatedCount, opt => opt.MapFrom(x => x.EventsParticipated.Count));
+        }
+    }
+}
